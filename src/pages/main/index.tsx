ca2ad96e85894b9525f4c39d4af44250936bd435
tulies <346461062@qq.com>
2020-04-27 23:27:57 +0800

    写个了hoc demo

--- src/pages/main/index.tsx
+++ src/pages/main/index.tsx
@@ -1,14 +1,17 @@
 import React from 'react'
 import Hello from '../../components/Hello'
-
+import DogShow from '../../components/ClassHoc/DogShow'
+import withLoader from '../../components/ClassHoc/withLoader';
 const Main: React.FC = (props)=>{
-
+    const WrapperDogShow = withLoader(DogShow, 'https://dog.ceo/api/breeds/image/random')
     return (
         <>
-            <h2 >Main Index</h2>
+            <h2 >下面这个是Hello组件，React.FC组件</h2>
             <Hello message="hello, 我是Hello组件！" ></Hello>
+            <hr/>
+            <h2 >下面这个是HOC组件，class形式的</h2>
+            <WrapperDogShow></WrapperDogShow>
         </>
     )
-
 }
 export default Main
