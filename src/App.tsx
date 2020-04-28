7bc1691a43953bcff9d7a3d3db6e1b4c4827c4ca
tulies <346461062@qq.com>
2020-04-28 22:46:23 +0800

    没有时间study

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter, Route, NavLink } from 'react-router-dom';
 import Main from './pages/main';
 import Ref from './pages/ref';
 import Effect from './pages/effect';
+import DiyHok from './pages/diy-hok';
 
 function App() {
   return (
@@ -13,10 +14,13 @@ function App() {
           <NavLink to="/" exact>首页</NavLink>
           <NavLink to="/effect">useEffect</NavLink>
           <NavLink to="/ref">useRef</NavLink>
+          <NavLink to="/diy-hok">diyHok</NavLink>
+
         </div>
         <Route path="/" exact><Main /></Route>
         <Route path="/ref" exact><Ref /></Route>
         <Route path="/effect" exact><Effect /></Route>
+        <Route path="/diy-hok" exact><DiyHok /></Route>
       </div>
     </BrowserRouter>
   );
---
+++ src/pages/diy-hok/index.tsx
@@ -0,0 +1,45 @@
+import React, {useState, useEffect, useRef} from 'react'
+
+// 自定义HOK演示。
+const DiyHok: React.FC = (props)=>{
+    // 每次render都是新的state。
+    const [like, setLike] = useState(0)
+    // ref会记住上一次的引用，
+    const likeRef = useRef(0)
+    useEffect(() => {
+        console.log('document title effect is running')
+        document.title = `点击了${like}次`
+    }, [like])
+
+
+    // 每次render的时候自动获得焦点
+    const domRef = useRef<HTMLInputElement>(null)
+    useEffect(() => {
+        if (domRef && domRef.current) {
+          domRef.current.focus()
+        }
+    })
+
+    const handleAlertClick = () =>{
+        setTimeout(() => {
+            // alert('you clicked on ' + like)
+            alert('you clicked on ' + likeRef.current)
+          }, 3000)
+    }
+    return (
+    <>
+        <h2>演示useRef的使用</h2>
+        <div>useRef</div>
+        <br/>
+        <br/>
+        <div>
+            <button onClick={()=>{setLike(like+1); likeRef.current++; console.log(likeRef)}}> {like} 👍 </button>
+            <button onClick={handleAlertClick}> alert like </button>
+        </div>
+        <div>
+            <input type="text" ref={domRef} />
+        </div>
+    </>
+    ) 
+}
+export default DiyHok
